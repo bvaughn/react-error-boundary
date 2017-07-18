5ddd85402a57f91f410c9a1d9e6b82d88edec5ba
Brian Vaughn <brian.david.vaughn@gmail.com>
2017-07-17 23:15:06 -0700

    Initial commit

---
+++ src/ErrorBoundary.js
@@ -0,0 +1,61 @@
+/** @flow */
+
+import React, { Component } from 'react';
+import ErrorBoundaryFallbackComponent from './ErrorBoundaryFallbackComponent';
+
+type Props = {
+  children?: any,
+  FallbackComponent: any,
+  onError?: (error: Error) => void,
+};
+
+type ErrorInfo = {
+  componentStack: string,
+};
+
+type State = {
+  error: ?Error,
+  info: ?ErrorInfo,
+};
+
+class ErrorBoundary extends Component {
+  props: Props;
+  state: State;
+
+  static defaultProps = {
+    FallbackComponent: ErrorBoundaryFallbackComponent,
+  };
+
+  constructor(props: Props, context: any) {
+    super(props, context);
+
+    this.state = {
+      error: null,
+      info: null,
+    };
+  }
+
+  unstable_handleError(error: Error, info: ErrorInfo):void {
+    // This method is a fallback for react <= 16.0.0-alpha.13
+    this.componentDidError(error, info);
+  }
+
+  componentDidError(error: Error, info: ErrorInfo):void {
+    this.setState({error, info});
+  }
+
+  render() {
+    const {children, FallbackComponent} = this.props;
+    const {error, info} = this.state;
+
+    if (error !== null) {
+      return (
+        <FallbackComponent componentStack={info && info.componentStack} error={error} />
+      );
+    }
+
+    return children;
+  }
+}
+
+export default ErrorBoundary;
---
+++ src/ErrorBoundaryFallbackComponent.js
@@ -0,0 +1,43 @@
+/** @flow */
+
+import React from 'react';
+
+type Props = {
+  componentStack: string,
+  error: Error,
+};
+
+const ErrorBoundaryFallbackComponent = ({ componentStack, error }: Props) => (
+  <div style={style} title={error.toString()}>
+    <svg style={svgStyle} viewBox="0 0 24 24" preserveAspectRatio="xMidYMid">
+      <path d={`M20,12A8,8 0 0,0 12,4A8,8 0 0,0 4,12A8,8 0 0,0 12,20A8,8 0 0,0 20,
+        12M22,12A10,10 0 0,1 12,22A10,10 0 0,1 2,12A10,10 0 0,1 12,2A10,10 0 0,1 22,
+        12M15.5,8C16.3,8 17,8.7 17,9.5C17,10.3 16.3,11 15.5,11C14.7,11 14,10.3 14,
+        9.5C14,8.7 14.7,8 15.5,8M10,9.5C10,10.3 9.3,11 8.5,11C7.7,11 7,10.3 7,9.5C7,
+        8.7 7.7,8 8.5,8C9.3,8 10,8.7 10,9.5M12,14C13.75,14 15.29,14.72 16.19,
+        15.81L14.77,17.23C14.32,16.5 13.25,16 12,16C10.75,16 9.68,16.5 9.23,
+        17.23L7.81,15.81C8.71,14.72 10.25,14 12,14Z`} />
+    </svg>
+  </div>
+);
+
+const style = {
+  height: '100%',
+  maxHeight: '100vh',
+  width: '100%',
+  maxWidth: '100vw',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  textAlign: 'center',
+  backgroundColor: '#C00',
+  color: '#FFF',
+  boxSizing: 'border-box',
+};
+
+const svgStyle = {
+  fill: 'currentColor',
+  flex: '1 1 auto',
+};
+
+export default ErrorBoundaryFallbackComponent;
