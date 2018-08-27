0bf67c68aabb7f124071f25523e992bf04638556
Rishabh Rao <rishabhsrao@gmail.com>
2018-08-27 16:58:24 -0700

    Setup Jest with Babel, React and Enzyme

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+// For a detailed explanation regarding each configuration property, visit:
+// https://jestjs.io/docs/en/configuration.html
+
+module.exports = {
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+
+  setupFiles: [
+    "./src/setupTests.js",
+  ],
+};
