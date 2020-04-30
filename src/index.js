c38c84b2d04c2db805bcc0ecd66f65168aeacd08
Kent C. Dodds <me+github@kentcdodds.com>
2020-04-30 13:07:28 -0600

    feat: support resetting the error state (#46)
    Closes #26 
    Closes #15 
    Closes #34 
    
    BREAKING CHANGE: No longer support default FallbackComponent.
    BREAKING CHANGE: Codebase is no longer written in Flow so flowtypes are no longer available (currently).

--- src/index.js
+++ src/index.js
@@ -1,8 +1,56 @@
-/** @flow */
+import React from 'react'
 
-import ErrorBoundaryFallbackComponent from './ErrorBoundaryFallbackComponent';
-import ErrorBoundary, {withErrorBoundary} from './ErrorBoundary';
+const initialState = {error: null, info: null}
+class ErrorBoundary extends React.Component {
+  state = initialState
+  resetErrorBoundary = () => this.setState(initialState)
 
-export default ErrorBoundary;
+  componentDidCatch(error, info) {
+    this.props.onError?.(error, info?.componentStack)
+    this.setState({error, info})
+  }
 
-export {ErrorBoundary, withErrorBoundary, ErrorBoundaryFallbackComponent};
+  render() {
+    const {error, info} = this.state
+    const {fallbackRender, FallbackComponent, fallback} = this.props
+
+    if (error !== null) {
+      const props = {
+        componentStack: info?.componentStack,
+        error,
+        resetErrorBoundary: this.resetErrorBoundary,
+      }
+      if (fallback) {
+        return fallback
+      } else if (fallbackRender) {
+        return fallbackRender(props)
+      } else if (FallbackComponent) {
+        return <FallbackComponent {...props} />
+      } else {
+        throw new Error(
+          'react-error-boundary requires either a fallback, fallbackRender, or FallbackComponent prop',
+        )
+      }
+    }
+
+    return this.props.children
+  }
+}
+
+function withErrorBoundary(Component, errorBoundaryProps) {
+  function Wrapped(props) {
+    return (
+      <ErrorBoundary {...errorBoundaryProps}>
+        <Component {...props} />
+      </ErrorBoundary>
+    )
+  }
+
+  // Format for display in DevTools
+  const name = Component.displayName || Component.name || 'Unknown'
+  Wrapped.displayName = `withErrorBoundary(${name})`
+
+  return Wrapped
+}
+
+export {ErrorBoundary, withErrorBoundary}
---
+++ tests/setup-env.js
@@ -0,0 +1,6 @@
+import '@testing-library/jest-dom/extend-expect'
+
+afterEach(() => {
+  jest.resetAllMocks()
+  jest.restoreAllMocks()
+})
