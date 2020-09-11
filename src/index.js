13f0ffd78707fdd2ab6a1e5a5e6e985cdc7e6fb1
Kent C. Dodds <me+github@kentcdodds.com>
2020-09-10 21:00:52 -0600

    fix: avoid double-render
    BREAKING CHANGE: This removes the `componentStack` in the props given to the `FallbackComponent` and `fallbackRender`

--- src/index.js
+++ src/index.js
@@ -3,52 +3,55 @@ import React from 'react'
 const changedArray = (a = [], b = []) =>
   a.length !== b.length || a.some((item, index) => !Object.is(item, b[index]))
 
-const initialState = {error: null, info: null}
+const initialState = {error: null}
 class ErrorBoundary extends React.Component {
   static getDerivedStateFromError(error) {
     return {error}
   }
 
   state = initialState
+  updatedWithError = false
   resetErrorBoundary = (...args) => {
     this.props.onReset?.(...args)
+    this.reset()
+  }
+
+  reset() {
+    this.updatedWithError = false
     this.setState(initialState)
   }
 
   componentDidCatch(error, info) {
-    this.props.onError?.(error, info?.componentStack)
-    this.setState({info})
+    this.props.onError?.(error, info)
   }
 
   componentDidUpdate(prevProps) {
-    const {error, info} = this.state
+    const {error} = this.state
     const {resetKeys} = this.props
-    if (
-      error !== null &&
-      info !== null &&
-      changedArray(prevProps.resetKeys, resetKeys)
-    ) {
+
+    // There's an edge case where if the thing that triggered the error
+    // happens to *also* be in the resetKeys array, we'd end up resetting
+    // the error boundary immediately. This would likely trigger a second
+    // error to be thrown.
+    // So we make sure that we don't check the resetKeys on the first call
+    // of cDU after the error is set
+    if (error !== null && !this.updatedWithError) {
+      this.updatedWithError = true
+      return
+    }
+
+    if (error !== null && changedArray(prevProps.resetKeys, resetKeys)) {
       this.props.onResetKeysChange?.(prevProps.resetKeys, resetKeys)
-      this.setState(initialState)
+      this.reset()
     }
   }
 
   render() {
-    const {error, info} = this.state
+    const {error} = this.state
     const {fallbackRender, FallbackComponent, fallback} = this.props
 
     if (error !== null) {
-      // we'll get a re-render with the error state in getDerivedStateFromError
-      // but we don't have the info yet, so just render null
-      // note that this won't be committed to the DOM thanks to our componentDidCatch
-      // so the user won't see a flash of nothing, so this works fine.
-      // the benefit of doing things this way rather than just putting both the
-      // error and info setState within componentDidCatch is we avoid re-rendering
-      // busted stuff: https://github.com/bvaughn/react-error-boundary/issues/66
-      if (!info) return null
-
       const props = {
-        componentStack: info?.componentStack,
         error,
         resetErrorBoundary: this.resetErrorBoundary,
       }
