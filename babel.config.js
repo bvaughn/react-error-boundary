005a408810e9b171ddf5cb8a1a62c35780be16eb
Brian Vaughn <brian.david.vaughn@gmail.com>
2023-06-10 09:13:13 -0400

    Replace @babel/preset-env with individual plug-ins like @babel/plugin-transform-optional-chaining

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  presets: ["@babel/preset-typescript", "@babel/preset-env"],
+  plugins: [
+    "@babel/plugin-transform-nullish-coalescing-operator",
+    "@babel/plugin-transform-optional-chaining",
+  ],
+  presets: ["@babel/preset-typescript"],
 };
