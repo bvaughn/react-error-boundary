abca669aff241eff44236249caea213fa5731068
Ben Huang <benjaminwong1985@gmail.com>
2024-08-24 21:49:54 -0400

    change: wrap the suppress-logging logic in the showBoundary function

--- src/useErrorBoundary.ts
+++ src/useErrorBoundary.ts
@@ -3,8 +3,8 @@ import { assertErrorBoundaryContext } from "./assertErrorBoundaryContext";
 import { ErrorBoundaryContext } from "./ErrorBoundaryContext";
 
 type StateError<TError> =
-  | (TError & { _suppressLogging: true })
-  | (Error & { _suppressLogging: true });
+  | (TError & { _suppressLogging: boolean })
+  | (Error & { _suppressLogging: boolean });
 
 type UseErrorBoundaryState<TError> =
   | { error: StateError<TError>; hasError: true }
@@ -15,36 +15,39 @@ export type UseErrorBoundaryApi<TError> = {
   showBoundary: (error: TError) => void;
 };
 
-function suppressLoggingForError<TError>(error: TError): StateError<TError> {
-  if (error != null) {
-    switch (typeof error) {
-      case "object": {
-        if (Object.isExtensible(error)) {
-          (error as StateError<TError>)._suppressLogging = true;
-          return error as StateError<TError>;
-        } else {
-          return Object.assign(Object.create(error as object), {
-            _suppressLogging: true as true,
-          });
-        }
-      }
-      default: {
-        return Object.assign(Error(error as string), {
-          _suppressLogging: true as true,
-        });
-      }
-    }
-  } else {
-    return Object.assign(Error(undefined), {
-      _suppressLogging: true as true,
-    });
-  }
-}
-
 export function useErrorBoundary<TError = any>(): UseErrorBoundaryApi<TError> {
   const context = useContext(ErrorBoundaryContext);
 
   assertErrorBoundaryContext(context);
+
+  const suppressLoggingForError = function <TError>(
+    error: TError
+  ): StateError<TError> {
+    const suppressLogging = {
+      _suppressLogging: context.suppressLogging as boolean,
+    };
+    if (error != null) {
+      switch (typeof error) {
+        case "object": {
+          if (Object.isExtensible(error)) {
+            (error as StateError<TError>)._suppressLogging =
+              context.suppressLogging;
+            return error as StateError<TError>;
+          } else {
+            return Object.assign(
+              Object.create(error as object),
+              suppressLogging
+            );
+          }
+        }
+        default: {
+          return Object.assign(Error(error as string), suppressLogging);
+        }
+      }
+    } else {
+      return Object.assign(Error(undefined), suppressLogging);
+    }
+  };
 
   const [state, setState] = useState<UseErrorBoundaryState<TError>>({
     error: null,
@@ -58,6 +61,9 @@ export function useErrorBoundary<TError = any>(): UseErrorBoundaryApi<TError> {
         setState({ error: null, hasError: false });
       },
       showBoundary: (error: TError) => {
+        if (context.suppressLogging) {
+          window.addEventListener("error", context.handleSuppressLogging);
+        }
         setState({
           error: suppressLoggingForError(error),
           hasError: true,
