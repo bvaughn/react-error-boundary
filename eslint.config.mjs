e1a6a0a8ea6da9fbcd7b44be66f144be1307d35c
Wonsuk Choi <sukvvon@gmail.com>
2024-12-22 06:03:47 +0900

    chore(eslint): migrate eslint configuration .eslintrc.js to eslint.config.mjs (#205)
    This pr converts the existing .eslintrc.js configuration file to the latest eslint format, eslint.config.mjs, and includes necessary updates to related libraries to support this change. a .nvmrc file specifying v20.9.0 has also been added to ensure compatibility with eslint’s minimum version requirements. the update ensures that the original eslint configuration is adapted to the flat config format while maintaining consistent functionality with typescript, react, and prettier plugins and recommended rules.
    
    **Why**:
    
    the main goal of this change is to adopt eslint’s latest configuration style, making maintenance easier and leveraging the flexibility of the flat config structure. this approach is expected to bring several benefits:
    
    * adoption of the latest module standard: the flat config format uses the latest ecmascript module (esm) standard, allowing for more flexible application of javascript features in configuration.
    * improved maintainability and extensibility: by updating plugins, we are now using versions optimized for flat config. this transition also prepares us to more easily adapt to future eslint version updates.
    * node version compatibility: a .nvmrc file specifying v20.9.0 has been added to ensure compatibility with [eslint’s minimum version requirements, as the latest eslint requires node.js version 18.18.0, 20.9.0, or 21.1.0 and above](https://eslint.org/docs/latest/use/getting-started#prerequisites). this change helps ensure that all environments using this repository align with the required node version.
    
    **How**:
    
    * configuration migration: all configurations from .eslintrc.js were transferred to eslint.config.mjs and adapted to the flat config format, ensuring functional parity with the previous setup.
    * library updates: major eslint-related plugins (typescript, react, prettier) were updated to their latest versions, supporting the new configuration style.
    * node version specification: a .nvmrc file was added with v20.9.0 to meet eslint’s minimum node version requirements, ensuring compatibility across environments.
    * environment and global variable consistency: the environment variables and global options (browser, es2021, node) from the previous configuration were applied consistently in the flat config, ensuring that the setup performs identically to the former .eslintrc.js configuration.
    
    this transition modularizes the eslint configuration with the latest standards, improving maintainability and allowing for greater flexibility in accommodating future eslint and plugin updates.

---
+++ eslint.config.mjs
@@ -0,0 +1,36 @@
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import reactPlugin from "eslint-plugin-react";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+
+export default tseslint.config(
+  {
+    ignores: ["dist"],
+  },
+  reactPlugin.configs.flat.recommended,
+  tseslint.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.es2021,
+      },
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+    plugins: {
+      react: reactPlugin,
+      "@typescript-eslint": tseslint.plugin,
+    },
+    rules: {
+      "react/no-did-update-set-state": "off",
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
+  eslintPluginPrettierRecommended
+);
