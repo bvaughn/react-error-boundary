483e98a941809c8bb44114e5197d38f743ac554b
Brian Vaughn <brian.david.vaughn@gmail.com>
2025-12-28 19:02:30 -0500

    Fix OG image URL

--- scripts/compile-docs.ts
+++ scripts/compile-docs.ts
@@ -1,8 +1,5 @@
 import { compileDocs } from "react-lib-tools/scripts/compile-docs.ts";
 
-// TODO Support hooks and HOCs?
-// This could also be useful for react-resizable-panels
-
 await compileDocs({
   componentNames: ["ErrorBoundary.tsx"],
   imperativeHandleNames: [],
